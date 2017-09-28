6811243b5f633b21014964bcc695f98094298eeb
soni17 <elb004@gmail.com>
2017-09-27 21:55:42 -0400

    rolls sorted

---
+++ rolls_sorted.rb
@@ -0,0 +1,14 @@
+
+arr = []
+
+10.times do
+  randomNumber = Random.rand(1..6)
+  arr.push(randomNumber)
+end
+
+sortedNumbers = arr.sort
+
+
+sortedNumbers.each do |item|
+  puts "The result of your roll is #{ item }"
+end
