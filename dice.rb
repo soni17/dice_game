637490a01a85e1821ae4c4d9712f32dec31571bd
soni17 <elb004@gmail.com>
2017-09-27 21:56:56 -0400

    two dices

---
+++ dice.rb
@@ -0,0 +1,7 @@
+
+randomNumber = Random.rand(1..6)
+randomNumber2 = Random.rand(1..6)
+
+puts "The result of your roll is #{randomNumber} and #{randomNumber2}"
+
+puts "Your total is #{randomNumber + randomNumber2}"
